8325670bba17084827814e416d3d220cf92e3ed8
Kanggwanju <xprtktm1@gmail.com>
2025-09-25 17:52:21 +0900

    chap16_3 배열 입력

---
+++ chap16_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int score[5];
+    
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d번 학생의 성적을 입력: ", i + 1);
+        scanf("%d", &score[i]);
+    }
+
+    printf("\n---------- 성적 입력 완료 ----------\n");
+
+    for (i = 0; i < 5; i++) {
+        printf("%d번 학생 점수: %d\n", (i + 1), score[i]);
+    }
+    
+    return 0;
+}
