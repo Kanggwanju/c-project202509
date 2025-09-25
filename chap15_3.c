54cbc88e24f6abe7979ee8f2cf64630e07d273f1
Kanggwanju <xprtktm1@gmail.com>
2025-09-25 16:15:17 +0900

    chap15_3 반복문 while

---
+++ chap15_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int i = 1;
+
+    while (i <= 5) {
+        printf("%d번째 외침! 우리 르세라핌 폼 미쳤다.\n", i);
+        i++;
+    }
+
+    // for (i = 1; i <= 5; i++) {
+    //     printf("%d번째 외침! 우리 르세라핌 폼 미쳤다.\n", i);
+    // }
+
+    return 0;
+}
