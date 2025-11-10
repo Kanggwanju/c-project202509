f04ed10e07818e7e5ebf62714c6083e2de5ab2a9
Kanggwanju <xprtktm1@gmail.com>
2025-11-11 02:06:10 +0900

    chap21_1~3 포인터

---
+++ chap21/chap21_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int a = 10;
+    int *p;  // 포인터 변수
+    // 포인터 변수의 크기는 8바이트로 항상 일정
+    p = &a;
+
+    printf("a의 주소: %p\n", &a);
+    printf("p의 값: %p\n", p);
+
+    printf("포인터 p로 찾아간 곳의 값: %d\n", *p);
+
+    return 0;
+}
