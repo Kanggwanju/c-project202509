14722da3d47307e24fa98d5cbfe88a0cf2e711e0
Kanggwanju <xprtktm1@gmail.com>
2025-11-10 20:55:54 +0900

    chap19_3 strncpy 버퍼 오버플로우 안전 문자열 복사

---
+++ chap19/chap19_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    
+    // strncpy(): 버퍼 오버플로우 안전하게 복사하기
+    char origin[] = "strawberry";
+    char copy[6];
+
+    // 복사 할 문자 수 지정 가능
+    strncpy(copy, origin, 5);
+
+    // strncpy는 널문자는 복사해오지 않음.
+    // 안전한 처리를 위해 마지막 칸에 수동으로 널문자를 넣어줌
+    copy[5] = '\0';
+    
+    printf("안전하게 복사된 문자열: %s\n", copy);
+
+    return 0;
+}
