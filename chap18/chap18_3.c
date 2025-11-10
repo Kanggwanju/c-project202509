09b682318c514d94e7f4e0f1db89585e3de55141
Kanggwanju <xprtktm1@gmail.com>
2025-11-10 18:29:22 +0900

    chap18_3 %s, strlen() 원리

---
+++ chap18/chap18_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // %s, strlen() 함수의 원리
+    // char fruit[] = "apple";
+    char fruit[] = {'a', 'p', 'p', 'l', 'e', '\0'};
+
+    int i = 0;
+    int count = 0;
+    while (fruit[i] != '\0') {
+        printf("%c", fruit[i]);
+        i++;
+        count++;
+    }
+    printf("\n단어 %s의 길이는 %d입니다.\n", fruit, count);
+
+    return 0;
+}
