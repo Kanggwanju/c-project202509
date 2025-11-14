7b8311894dbd21c8d13f84c16e3f45e8174625fc
Kanggwanju <xprtktm1@gmail.com>
2025-11-14 23:13:12 +0900

    chap25_4 배열 합 구하기 풀이

--- chap25/chap25_4.c
+++ chap25/chap25_4.c
@@ -15,5 +15,13 @@ int main(void) {
     int my_arr[] = {10, 20, 30, 40, 50};
     printf("배열의 총 합: %d\n", sum_array(my_arr, 5)); // 결과: 150
 
+    // sum(5) (150)
+    // ㄴarr[4] + sum(4) (50 + 100)
+    //            ㄴarr[3] + sum(3) (40 + 60)
+    //                       ㄴarr[2] + sum(2) (30 + 30)
+    //                                  ㄴarr[1] + sum(1) (20 + 10)
+    //                                             ㄴarr[0] + sum(0) (10 + 0)
+    //                                                        ㄴ0
+
     return 0;
 }
