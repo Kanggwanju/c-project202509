e548bb6c1c6c753f1196eecca631a79a12859d88
Kanggwanju <xprtktm1@gmail.com>
2025-11-18 23:26:52 +0900

    chap27_2 메모리 free 후 주소나 값 문제

--- chap27/chap27_2.c
+++ chap27/chap27_2.c
@@ -27,10 +27,16 @@ int main(void) {
         printf("%d번 학생의 점수: %d점\n", i + 1, scores[i]);
     }
 
+    printf("free전 scores의 주소: %p\n", scores);
+
     // free 하는 것을 깜빡했을 때
     // 그 메모리는 프로그램이 끝날 때까지 아무도 못 쓰는 '유령 공간'이 됨
     // 누수가 쌓이면, 프로그램 전체가 메모리 부족으로 멈춤
     free(scores);
+    // scores = NULL; free후 명시적으로 NULL 값 할당하는게 좋음
+
+    printf("free후 scores의 주소: %p\n", scores);
+    printf("free후 scores[0]의 값: %d\n", scores[0]); // free후의 값이나 주소는 예측 불가능
 
     return 0;
 }
