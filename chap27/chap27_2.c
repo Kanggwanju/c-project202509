cf620c3652e0034ab650ccd539daa14452106d3b
Kanggwanju <xprtktm1@gmail.com>
2025-11-18 23:08:14 +0900

    chap27_2 scanf 동적 메모리 할당 예시

---
+++ chap27/chap27_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+    int student_count;
+
+    printf("성적을 입력할 학생 수 : ");
+    scanf("%d", &student_count);
+
+    // 학생 수만큼 배열의 공간을 할당
+    int *scores = (int*) malloc(student_count * sizeof(int));
+
+    if (scores == NULL) {
+        printf("메모리가 할당되지 않았습니다.");
+        return 1;
+    }
+
+    // 입력받기
+    for (int i = 0; i < student_count; i++) {
+        printf("%d번 학생의 성적: ", i + 1);
+        scanf("%d", &scores[i]);
+    }
+
+    printf("\n==== 입력된 성적 ====\n");
+    for (int i = 0; i < student_count; i++) {
+        printf("%d번 학생의 점수: %d점\n", i + 1, scores[i]);
+    }
+
+    free(scores);
+
+    return 0;
+}
