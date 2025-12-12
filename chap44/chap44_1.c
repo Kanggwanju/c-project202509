5032e96e8bacf4673cffc32883cb292354a79058
Kanggwanju <xprtktm1@gmail.com>
2025-12-12 17:54:50 +0900

    chap44_1 기출문제 2023년 2회차 switch 폴쓰루

---
+++ chap44/chap44_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 2023년 2회차, switch 폴쓰루
+int main(void) {
+    
+    int n[3] = {73, 95, 82};
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++) {
+        sum += n[i];
+    }
+
+    // 250 / 30 -> 몫: 8
+    // 정답: BCD
+    switch(sum / 30) {
+        case 10:
+        case 9: printf("A");
+        case 8: printf("B");
+        case 7: 
+        case 6: printf("C");
+        default: printf("D");
+    }
+
+    return 0;
+}
